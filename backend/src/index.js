61161d9c2823b1406bb69609d63604d4a1b7812d
zainab7681051 <zainab7681051@gmail.com>
2022-10-18 23:44:23 +0300

    m

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ const config = require('./config')
 
 const app = express();
 let corsOption = {
-	origin: "https://free-documentaries.onrender.com"
+	origin: "https://free-documentaries.onrender.com/"
 }
 
 app.use(cors(corsOption))
